26e52cbe91f49cb9fb1036c0782ea56c6de2bac5
presioncup <infotorneoporrino@gmail.com>
2025-05-31 16:41:03 +0200

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+
+function App() {
+  return (
+    <div className="bg-black text-white p-10 text-center">
+      <h1 className="text-4xl font-bold text-green-400">✅ Tailwind está funcionando</h1>
+    </div>
+  );
+}
+
+export default App;
+
